5d66eaba1a403758275625e88917658a3af51214
Ashizriati <ashizriati211@sma.belajar.id>
2024-11-05 12:30:36 +0800

    Add files via upload

---
+++ prog1.py
@@ -0,0 +1,6 @@
+# pembelajaran algoritma
+print ("nama saya adalah ashizriati")
+print ("saya adalah seorang pelajar")
+print ("nim saya 24241115")
+print ("saya berasal dari dompu")
+print ("nim keberutungan saya 2415")
