6c19a69c949bcf49d496fbbc52ed71a4a768841b
Ashizriati <ashizriati211@sma.belajar.id>
2024-12-08 09:52:52 +0800

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,32 @@
+# ARIMATIKA ( X : + - ** % // )
+
+a = 24
+b = 15
+
+#operasi kali X
+hasil = a * b 
+print("nilai dari a * b =",hasil)
+
+#operasi bagi :
+hasil = a / b 
+print("nilai dari a / b =",hasil)
+
+#operasi tambah +
+hasil = a + b 
+print("nilai dari a + b =",hasil)
+
+#operasi kurang -
+hasil = a - b 
+print("nilai dari a - b=",hasil)
+
+#operasi perpangkatan **
+hasil = a ** b 
+print("nilai dari a ** b=",hasil)
+
+#modulus (sisa bagi) %
+hasil = a % b 
+print("nilai dari a mod b=",hasil)
+
+#floor dividion //
+hasil = a // b 
+print("nilai dari a flor b=",hasil)
