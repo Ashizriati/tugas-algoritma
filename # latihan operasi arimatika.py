a50db02ffae1e3d7acf37b282b07423d2c22021c
Ashizriati <ashizriati211@sma.belajar.id>
2024-12-13 22:10:20 +0800

    Add files via upload

---
+++ # latihan operasi arimatika.py
@@ -0,0 +1,42 @@
+# latihan operasi arimatika
+
+# masukan nilai dalam meter
+# nilai dalam milimeter =
+# nilai dalam centimeter =
+# nilai dalam kilometer =
+
+nilai = int (input("masukan nilai dalam meter "))
+
+hasil = nilai * 1000
+print ("nilai dalam milimeter :", hasil)
+
+nilai = int ( input ("masukan nilai dalam centimeter") )
+
+hasil = nilai * 1000
+print ("nilai dalam centimeter :", hasil )
+
+nilai = int (input ("masukan nilai dala kilometer"))
+
+hasil = nilai * 1000
+print ( " nilai dalam kilometer :", hasil)
+
+
+# suhu celcius
+# suhu dalam fahrenheit
+# suhu dalam kelvin
+# suhu dalam reamur
+
+nilai = int (input (" masukan nilai suhu fahrenheit:",))
+
+hasil = 15
+print ("nilai dalam suhu fahrenheit :",hasil)
+
+nilai = int (input (" masukan nilai suhu kelvin:",))
+
+hasil = 24
+print ("nilai dalam suhu kelvin :",hasil)
+
+nilai = int (input (" masukan nilai suhu reamur:",))
+
+hasil = 24
+print ("nilai dalam suhu reamur :",hasil)
